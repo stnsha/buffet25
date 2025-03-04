6fc9c924727b36f1fe07603dd779959ffd5d0c9e
Siti Anasuha <anasuharosli@gmail.com>
2025-03-04 10:41:43 +0800

    feat: auto calc pax'

--- app/Http/Controllers/VenueController.php
+++ app/Http/Controllers/VenueController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Capacity;
+use App\Models\OrderDetails;
 use App\Models\Venue;
 use Illuminate\Http\Request;
 
@@ -13,54 +15,48 @@ class VenueController extends Controller
     public function index($venue_id)
     {
         $venue = Venue::find($venue_id);
+        $this->calcPax();
         return view('venues.index', compact('venue', 'venue_id'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function calcPax()
     {
-        //
-    }
+        $order_details = OrderDetails::with('order')
+            ->whereHas('order', function ($query) {
+                $query->where('status', 2);
+            })
+            ->get()
+            ->groupBy('order_id')
+            ->map(function ($details) {
+                return [
+                    'venue_id' => $details->first()->order->venue_id,
+                    'total_quantity' => $details->sum('quantity')
+                ];
+            });
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        $total_by_capacity = collect($order_details)
+            ->groupBy('venue_id')
+            ->map(function ($orders) {
+                return [
+                    'total_quantity' => $orders->sum('total_quantity')
+                ];
+            });
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Venue $venue)
-    {
-        //
-    }
+        foreach ($total_by_capacity as $capacity_id => $tc) {
+            $capacity = Capacity::find($capacity_id);
+            $full_capacity = $capacity->full_capacity;
+            $total_paid = $capacity->total_paid;
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Venue $venue)
-    {
-        //
-    }
+            $total_paid = $tc['total_quantity'];
+            $updated_capacity = $full_capacity - $total_paid;
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Venue $venue)
-    {
-        //
-    }
+            $capacity->available_capacity = $updated_capacity;
+            $capacity->total_paid = $total_paid;
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Venue $venue)
-    {
-        //
+            if ($updated_capacity == 0) {
+                $capacity->status = 2;
+            }
+            $capacity->save();
+        }
     }
 }
